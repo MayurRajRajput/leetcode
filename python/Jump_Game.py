5bb0a678545960c247435d66244dc7566de48216
MAYUR RAJ <mayurrajrajput07@gmail.com>
2025-05-13 23:38:39 +0530

    day 57

---
+++ python/Jump_Game.py
@@ -0,0 +1,34 @@
+# Jump Game
+""" 
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+"""
+from typing import List
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+            
+        return True
+nums = [2,3,1,1,4]
+sol =Solution()
+res = sol.canJump(nums)
+print(res)  
+""" 
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+"""
---
+++ python/Spiral_Matrix.py
@@ -0,0 +1,44 @@
+# Spiral Matrix
+""" 
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+"""
+from typing import List
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom + 1):
+                res.append(matrix[i][right])
+            right -= 1
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
+        return res
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+sol = Solution()
+res = sol.spiralOrder(matrix)
+print(res)
+""" 
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+"""
