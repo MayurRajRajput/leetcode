39da0059d9f2afa9a2ed32abf8f8b229fff97b59
MAYUR RAJ <mayurrajrajput07@gmail.com>
2025-06-22 22:23:45 +0530

    day 97

---
+++ JavaScript/The_K_Weakest_Rows_in_a_Matrix.js
@@ -0,0 +1,65 @@
+// The K Weakest Rows in a Matrix
+/*
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+*/
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+
+var kWeakestRows = function(mat, k) {
+    let rowStrengths = mat.map((row, i) => [row.reduce((a, b) => a + b, 0), i]);
+    rowStrengths.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+    return rowStrengths.slice(0, k).map(([_, i]) => i);
+};
+ mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]]
+k = 3
+console.log(kWeakestRows(mat,k))
+/*
+Example 1:
+
+Input: mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+Output: [2,0,3]
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 2 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 2 
+- Row 4: 5 
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+Example 2:
+
+Input: mat = 
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]], 
+k = 2
+Output: [0,2]
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 1 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 1 
+The rows ordered from weakest to strongest are [0,2,3,1].
+ 
+*/
